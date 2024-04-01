f4be090d6c8dfc2476f624a46856795a740bda22
Jonathan Munoz <jmmunoz.ap@gmail.com>
2024-04-01 13:21:29 -0500

    Initial

---
+++ aes/aes_block_128_tb.sv
@@ -0,0 +1,26 @@
+module aes_round #(parameter int SIZE = 128)
+(
+    input wire clock,
+    input wire [SIZE - 1:0] data,
+    input wire [63:0] nonce,
+    input wire [SIZE - 1:0] round_key,
+    output logic [SIZE - 1:0]  enc_data
+);
+
+
+    
+endmodule: aes_round
+
+
+module aes_block #(parameter int SIZE = 128)
+(
+    input wire clock,
+    input wire [SIZE - 1:0] data,
+    input wire [63:0] nonce,
+    input wire [SIZE - 1:0] key,
+    output logic [SIZE - 1:0]  enc_data
+);
+
+    
+    
+endmodule: aes_block
